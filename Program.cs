0c947afd7499201ac47a20367c532af47799da1a
callegarifede1 <callegarifede1@gmail.com>
2022-05-20 12:36:26 -0300

    Creacion del programa

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+Console.WriteLine("Ingrese un valor");
+int nuevoValor = Int32.Parse(Console.ReadLine ());
+
+Random numero = new Random();
+int numeroSecreto = numero.Next(0, 21);
+
+int intentos = 0;
+
+while(nuevoValor != numeroSecreto ){
+    if(nuevoValor < numeroSecreto){
+        Console.WriteLine("Debes ingresar un numero mayor");
+        nuevoValor = int.Parse(Console.ReadLine ());
+    }
+    else if (nuevoValor > numeroSecreto){
+        Console.WriteLine("Debes ingresar un numero menor");
+        nuevoValor = int.Parse(Console.ReadLine ());
+    }
+    intentos++; 
+}
+Console.WriteLine("Felicidades!! Adivinaste el numero!!");
+Console.WriteLine($"Tu numero de intentos es : {intentos}");
+
